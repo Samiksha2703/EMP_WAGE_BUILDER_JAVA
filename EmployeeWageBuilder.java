6314440863b7bbd4aa7711031de5343ff931e289
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-09 19:46:39 +0530

    [Samiksha].ADD addition part time code is added to the calculate employee wage code

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,7 +3,8 @@ public class EmployeeWageBuilder {
 public static void main (String args []){
 
 //Constants
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 
 //variables
@@ -11,9 +12,11 @@ int empHrs = 0;
 int empWage = 0;
 
 //Computaion
-double empCheck = Math.floor(Math.random() * 10) % 2;
+double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
+	else if (empCheck == IS_PART_TIME)
+		empHrs = 4;
         else
                 empHrs = 0;
         empWage = empHrs * EMP_RATE_PER_HOUR;
