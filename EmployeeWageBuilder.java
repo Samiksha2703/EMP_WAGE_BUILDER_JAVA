139adc5530fe23ccebe662ef6176853e6555385d
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-09 20:23:44 +0530

    [Samiksha].ADD calculate monthly employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,30 +4,35 @@ public class EmployeeWageBuilder {
 public static final int IS_PART_TIME = 1;
 public static final int IS_FULL_TIME = 2;
 public static final int EMP_RATE_PER_HOUR = 20;
+public static final int NUM_OF_WORKING_DAYS = 20;
 
 public static void main (String args []){
 
 //variables
 int empHrs = 0;
 int empWage = 0;
+int totalEmpWage = 0;
 
 //Computaion
-int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	switch (empCheck){
-	case IS_FULL_TIME :
- 		empHrs = 8;
-        	break;
+for (int day = 0; day < NUM_OF_WORKING_DAYS ; day++){
+	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck){
+		case IS_FULL_TIME :
+ 			empHrs = 8;
+        		break;
 
-	case IS_PART_TIME :
- 		empHrs = 4;
-        	break;
+		case IS_PART_TIME :
+ 			empHrs = 4;
+        		break;
 
-	default :
-		empHrs = 0;
-        	break;
-	}
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("EmpWage : " +empWage);
+		default :
+			empHrs = 0;
+        		break;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
+		System.out.println("EmpWage : " +empWage);
+		}
+		System.out.println("Total Emp Wage : " +totalEmpWage);
 	}
 }
-
