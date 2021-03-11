ec6c32ec9db449b26160944b88864b0c93696b3a
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-11 21:35:39 +0530

    [Samiksha].ADD code to calculate for different companies

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,46 +1,43 @@
 public class EmployeeWageBuilder {
 
-	// Class Variables
+	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_NUM_HOURS = 100;
 
-	// Method to calculate the employee wage
-	public void calculateEmpWage() {
-		// Method variables
-		int totalEmpWage = 0;
+	//Method to calculate employee wage
+	public static void calculateEmpWage(String company, int empRatePerHour, int numOfWorkingDays,
+			int maxHoursPerMonth) {
+
+		// Variables
+		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_NUM_HOURS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+
+		// Computation
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
-			int empHrs = 0;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
-
 			case IS_PART_TIME:
 				empHrs = 4;
 				break;
-
 			default:
 				empHrs = 0;
 				break;
 			}
 			totalEmpHrs += empHrs;
-			int empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("EmpWage : " + empWage);
+			System.out.println("Day# : " + totalWorkingDays + " Emp Hrs : " + empHrs);
 		}
-		System.out.println("Total Emp Wage : " + totalEmpWage);
+
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for company " + company + " is : " + totalEmpWage);
 	}
 
 	public static void main(String args[]) {
-		EmployeeWageBuilder employeeWage = new EmployeeWageBuilder();
-		employeeWage.calculateEmpWage();
+		calculateEmpWage("D-mart", 20, 20, 100);
+		calculateEmpWage("Tech-Mahendra", 15, 18, 85);
 	}
-
 }
